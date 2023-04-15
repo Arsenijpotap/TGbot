9dca08bbb2d6de08f7b81618ddc87ccebf0a987e
Your Name <you@example.com>
2023-04-15 12:39:40 +0300

    new12345

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import telebot
+import requests
+from bs4 import BeautifulSoup
 from pyowm import OWM
 from pyowm.utils.config import get_default_config
 language = get_default_config()
@@ -10,9 +12,15 @@ keybord1 = telebot.types.ReplyKeyboardMarkup(True)
 keybord1.row('найти контент по актеру', 'найти актеров по контенту', 'список избранного', 'завершить')
 keybord2 = telebot.types.ReplyKeyboardMarkup(True)
 keybord2.row('включить уведомления', 'выключить уведомления', 'обратно')
+contents= requests.get("https://hd.kinopoisk.ru/special/kino2mfreeby?utm_source=google_search&utm_medium=paid_performance&utm_campaign=19596590067|MSCAMP-3_[KP-P]_{WS:S}_BY-149_goal-PL_Category-Brand&utm_content=INTid|kwd-3762169347|cid|19596590067|gid|146370538478|aid|645903043567|pos||src|g_|dvc|c&utm_term=kinopoisk.[b]&gclid=Cj0KCQjwlumhBhClARIsABO6p-wJlnR8Dmz1nr7dH-B_zLYWWTpQ7bVTCplTeLyKQARwDUM5hx8iCngaAs6bEALw_wcB")
+soup = BeautifulSoup(contents.content.decode('utf-8'), "html.parser")
+print(contents.status_code, soup.title.text)
+
+
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
-   bot.send_message(message.chat.id, 'Здравствуйте, в моем боте вы можете найти необходимый фильм, сериал или актера.\n Также можете добавлять контент в избранное и получать уведомления по необходимой информации',reply_markup=keybord1)
+   bot.send_message(message.chat.id, 'Здравствуйте, в моем боте вы можете найти необходимый фильм, сериал или актера.\n Также можете добавлять контент в избранное и получать уведомления по необходимой информации', soup.title.text, reply_markup=keybord1)
 @bot.message_handler(content_types=['text'])
 def start_message1(message):
    if message.text=='найти актеров по контенту':
